07e131b98678dd8dd8cc44bfb3eef312616652bd
BBB <rkrasowski@yahoo.com>
2013-12-27 21:22:15 +0000

    readMail reads files in directory New

--- readMail.pl
+++ readMail.pl
@@ -2,4 +2,26 @@
 use strict;
 use warnings;
 
+my $newDirectory = "/home/ubuntu/Mail/New/";
+
+
+
 print "Mail reader started.\n";
+
+
+
+
+opendir (my $NEW , $newDirectory) or die "Can not open new directory: $!\n";
+my @newFiles = grep !/^\./, readdir($NEW);
+
+my $smsNum = 1;
+foreach(@newFiles)
+	{
+		print "Mail number $smsNum : $_\n";
+		$smsNum++;
+	}
+
+
+
+close ($NEW);
+

1de10096b20f660c04c9dcd80eca0a0d771512c2
BBB <rkrasowski@yahoo.com>
2013-12-28 02:42:11 +0000

    Transfer works, delate - not yes, few bugs still present

--- readMail.pl
+++ readMail.pl
@@ -9,7 +9,7 @@ $Term::ANSIColor::AUTORESET = 1;
 
 my $newDirectory = "/home/ubuntu/Mail/New/";
 my $readDirectory = "/home/ubuntu/Mail/Read/";
-
+STARTMESSAGES:
 
 opendir (my $NEW , $newDirectory) or die "Can not open new directory: $!\n";
 my @newFiles = grep !/^\./, readdir($NEW);
@@ -26,14 +26,14 @@ my $unreadNumber = @newFiles;
 my $totalNumber = @totalArray;
 my $totalArray;
 
-print "     Press 1 to check New Mail\n     Press 2 to check Read Mail\n     Press 3 to write Mail\n     Press X to exit\n\n";
+print BOLD BLUE "\nMESSAGES MENAGER\n     Press 1 to check Messages\n     Press 2 to write Message\n     Press X to exit\n\n";
 my $newFiles;
 
 
 while(<>)
 	{
 		if ($_ == 1)
-			{
+			{	MESSAGEMENAGER:
 				
 				print "Messages:\n\n";
 				my $smsNum = 1;
@@ -49,7 +49,7 @@ while(<>)
 						print BOLD YELLOW"Old message number $j: $totalArray[$j-1]\n"; 
 
 					}
-			print "\nEnter message number that you want to read and press ENTER\n";
+			   	print "\nEnter message number that you want to read and press ENTER\n";
 				
 				while(<>)
 					{
@@ -57,10 +57,47 @@ while(<>)
 							{
 								my $arrayNum = $_ - 1;
 								my $sms = `cat /home/ubuntu/Mail/New/$newFiles[$arrayNum]`;
+
+								# move message to Read folder 
+                                                                rename "/home/ubuntu/Mail/New/$newFiles[$arrayNum]","/home/ubuntu/Mail/Read/$newFiles[$arrayNum]";
+		
+
 								chomp $_;
-								print "Your message  number $_:\n\n";
+								print "\nYour message  number $_:\n\n";
 								print BOLD YELLOW "$sms\n\n";
-								print BOLD BLUE "Press X to returm the menu\nPress email number to read another email\n\n";
+								print BOLD BLUE "Press 1 to reply to this message\nPress 2 to return to messages list\nPress 3 to erase this message\n\n";
+								while(<>)
+									{
+										if ($_ == 1)
+											{
+												print BOLD GREEN "Write your message and press ENTER\n";
+											}
+
+										if ($_== 2)
+											{
+												goto STARTMESSAGES;
+											}
+										if ($_== 3)
+											{
+												print BOLD RED "Are you sure, you want to delay that message? Press Y or N !!\n\n";
+												while(<>)
+													{
+														if ($_ =~ m/Y/)	
+															{
+																print "Message dalated\n\n";
+																goto STARTMESSAGES;
+															}
+														if ($_  =~ m/N/)
+															{
+																print "Message NOT delated\n\n";
+																goto STARTMESSAGES;
+															}
+														else {print "Try again , Y or N\n\n";}
+													
+													}
+											}
+										else {print "Try again, 1, 2 or 3 only\n\n";}
+									}
 							}
 						
 						if ($_ > $unreadNumber and $_ <= $totalNumber)
@@ -68,12 +105,11 @@ while(<>)
                                                                 my $arrayNum = $_ - 1 - $unreadNumber;
                                                                 my $sms = `cat /home/ubuntu/Mail/Read/$readFiles[$arrayNum]`;
                                                                 chomp $_;
-                                                                print "Your message  number $_:\n\n";
+                                                                print "\nur message  number $_:\n\n";
                                                                 print BOLD YELLOW "$sms\n\n";
-                                                                print BOLD BLUE "Press X to returm the menu\nPress email number to read another email\n\n";
+                                                                print BOLD BLUE "Press 1 to reply to this message\nPress 2 to return to messages list\nPress x to erase this message\n\n";
                                                         }
-
-
+						else { print "Wrong number\n";}
 					}
 
 
